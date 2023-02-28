77452768db097f9aa323d9f3b0e6588abb12315c
abhayrana2139 <rana2139@gmail.com>
2023-02-28 13:40:40 +0530

    commit

---
+++ src/blogs.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const blogs = () => {
+  return (
+    <div className=' bg-red-500 h-auto w-auto'>
+
+
+    <div>
+        </div>
+
+
+
+
+      
+    </div>
+  )
+}
+
+export default blogs
